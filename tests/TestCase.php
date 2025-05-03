ff20c5168f383226adc6515f2faf1d271e40e4c3
Matt Daneshvar <matt.daneshvar@gmail.com>
2025-05-03 09:08:49 -0600

    Create dummy tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,21 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    #[Test, DataProvider('provider')]
+    public function it_works(): void
+    {
+        usleep(10 * 1000);
+
+        $this->assertTrue(true);
+    }
+
+    public static function provider(): array
+    {
+        return array_fill(0, 100, []);
+    }
 }
