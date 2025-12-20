9ca07f80d93ff606e6f90ce382fef63f73e8f6a9
Matt Daneshvar <matt.daneshvar@gmail.com>
2025-12-20 08:53:19 -0600

    Increase database reads and writes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,27 +2,50 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Database\Factories\UserFactory;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 
 abstract class TestCase extends BaseTestCase
 {
+    use DatabaseTransactions;
+
     #[Test, DataProvider('provider')]
-    public function it_works($foo): void
+    public function it_works($writeCount, $readCount): void
     {
-        UserFactory::new()->create();
+        UserFactory::new()->count($writeCount)->create();
 
-        usleep(1 * 1000);
+        for($i = 0; $i < $readCount; $i++) {
+            User::query()->inRandomOrder()->first();
+        }
 
-        $this->assertTrue(true);
+        $this->get('/')->assertSuccessful();
     }
 
     public static function provider(): array
     {
-        return array_fill(0, 100, [
-            'foo' => 'bar',
-        ]);
+        $lightLoad = [
+            'writeCount' => 1,
+            'readCount' => 1,
+        ];
+
+        $mediumLoad = [
+            'writeCount' => 10,
+            'readCount' => 10,
+        ];
+
+        $heavyLoad = [
+            'writeCount' => 100,
+            'readCount' => 100,
+        ];
+
+        return [
+            ...array_fill(0, 100, $lightLoad),
+            ...array_fill(0, 100, $mediumLoad),
+            ...array_fill(0, 100, $heavyLoad),
+        ];
     }
 }
